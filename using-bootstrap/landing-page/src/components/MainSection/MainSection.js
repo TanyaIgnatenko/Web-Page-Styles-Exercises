5dac91833ddc3540b9d1acc42f17722ab5ac9371
TanyaIgnatenko <alisainy@gmail.com>
2018-11-10 22:54:19 +0700

    do small fix

--- using-bootstrap/landing-page/src/components/MainSection/MainSection.js
+++ using-bootstrap/landing-page/src/components/MainSection/MainSection.js
@@ -9,7 +9,9 @@ const MainSection = props => (
             <h1 className="display-2"><strong>Full Stack Conf</strong></h1>
             <p className="lead"><strong>A one-day conference about All Things JavaScript</strong></p>
             <div className="btn-group mt-2" role="group" aria-label="Basic example">
-                <button type="button" className="btn btn-primary btn-lg" data-toggle="modal" data-target="#register">Register now</button>
+                <button type="button" className="btn btn-primary btn-lg" data-toggle="modal" data-target="#registerForm">
+                    Register now
+                </button>
                 <a type="button" className="btn btn-light btn-lg" href='#speakers'>See speakers</a>
             </div>
         </div>
