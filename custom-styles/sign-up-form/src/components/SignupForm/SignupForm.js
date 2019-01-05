a9a7127078d38db13e01c516336192df1597cc81
TanyaIgnatenko <alisainy@gmail.com>
2019-01-05 17:21:45 +0700

    Add linters

--- custom-styles/sign-up-form/src/components/SignupForm/SignupForm.js
+++ custom-styles/sign-up-form/src/components/SignupForm/SignupForm.js
@@ -7,41 +7,43 @@ import PasswordIcon from '@/assets/images/lock.png';
 
 import styles from './SignupForm.scss';
 
-const SignupForm = (props) => (
+function SignupForm() {
+  return (
     <div className={styles.container}>
-        <h1>Create account</h1>
-                <ul className={styles.socialMediaContainer}>
-                    <li className={styles.googlePlusIcon}>
-                        <a href='#'>
-                            <i className='fab fa-google-plus-g'></i>
-                        </a>
-                    </li>
-                    <li className={styles.facebookIcon}>
-                        <a href='#'>
-                            <i className='fab fa-facebook-f'></i>
-                        </a>
-                    </li>
-                    <li className={styles.twitterIcon}>
-                        <a href='#'>
-                            <i className='fab fa-twitter'></i>
-                        </a>
-                    </li>
-                </ul>
-        <p>Or use email for registration:</p>
-        <div className={classNames(styles.innerAddon, styles.leftAddon)}>
-            <img src={UserIcon} className={styles.inputIcon}/>
-            <input type='text' placeholder='Name'/>
-        </div>
-        <div className={classNames(styles.innerAddon, styles.leftAddon)}>
-            <img src={EmailIcon} className={styles.inputIcon}/>
-            <input type='email' placeholder='Email'/>
-        </div>
-        <div className={classNames(styles.innerAddon, styles.leftAddon)}>
-            <img src={PasswordIcon} className={styles.inputIcon}/>
-            <input type='password' placeholder='Password'/>
-        </div>
-        <button className={styles.signupBtn}>SIGN UP</button>
+      <h1>Create account</h1>
+      <ul className={styles.socialMediaContainer}>
+        <li className={styles.googlePlusIcon}>
+          <a href='#'>
+            <i className='fab fa-google-plus-g' />
+          </a>
+        </li>
+        <li className={styles.facebookIcon}>
+          <a href='#'>
+            <i className='fab fa-facebook-f' />
+          </a>
+        </li>
+        <li className={styles.twitterIcon}>
+          <a href='#'>
+            <i className='fab fa-twitter' />
+          </a>
+        </li>
+      </ul>
+      <p>Or use email for registration:</p>
+      <div className={classNames(styles.innerAddon, styles.leftAddon)}>
+        <img src={UserIcon} className={styles.inputIcon} />
+        <input type='text' placeholder='Name' />
+      </div>
+      <div className={classNames(styles.innerAddon, styles.leftAddon)}>
+        <img src={EmailIcon} className={styles.inputIcon} />
+        <input type='email' placeholder='Email' />
+      </div>
+      <div className={classNames(styles.innerAddon, styles.leftAddon)}>
+        <img src={PasswordIcon} className={styles.inputIcon} />
+        <input type='password' placeholder='Password' />
+      </div>
+      <button className={styles.signupBtn}>SIGN UP</button>
     </div>
-);
+  );
+}
 
 export default SignupForm;
