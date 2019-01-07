3b716294ada9a9cfae7c884147ce893b241c88f4
TanyaIgnatenko <alisainy@gmail.com>
2019-01-07 21:08:13 +0700

    Simplify project

--- custom-styles/yandex-blitz/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-
-import Logo from './components/Logo/Logo';
-
-import './index.scss';
-
-const logoNode = document.getElementById('logo-node');
-render(<Logo />, logoNode);
